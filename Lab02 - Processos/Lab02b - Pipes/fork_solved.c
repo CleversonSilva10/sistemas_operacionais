07ab7f57501d2d61bed530b3d46fb3910dcdba2c
Gustavo Teixeira <gustavoteixeira684@gmail.com>
2023-03-15 20:07:02 -0300

    Inserindo Lab02b - Pipes

---
+++ Lab02 - Processos/Lab02b - Pipes/fork_solved.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int value = 5;
+
+int main()
+{
+	pid_t pid;
+  int fd[2];
+  
+  if(pipe(fd) < 0){
+    printf("n foi");
+  }
+	pid = fork();
+
+	if (pid == 0) { /* child process */
+		printf("Entrei no filho!\n");
+		value += 15;
+    write(fd[1], &value, sizeof(value));
+		printf ("CHILD: value = %d\n",value); /* LINE A */
+		return 0;
+	}
+	else if (pid > 0) { /* parent process */
+		wait(NULL);
+    read(fd[0], &value, sizeof(value));
+		printf ("PARENT: value = %d\n",value); /* LINE A */
+		return 0;
+	}
+}
---
+++ Lab02 - Processos/Lab02b - Pipes/readme.md
@@ -0,0 +1,3 @@
+Para resolver esse problema nós utilizamos o conceito de pipe()
+
+Com esse conceito, nós criamos um canal de comunicação entre o processo pai e o processo filho. Com ele, o processo filho pode fazer modificações nas variáveis e o processo pai pode lê-las
