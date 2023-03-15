cf912ac03d1757dcefcea3d7523ae2100de23006
GustavoTeixeira684 <73202148+GustavoTeixeira684@users.noreply.github.com>
2023-03-15 20:09:01 -0300

    Ajustando

--- Lab02 - Processos/Lab02b - Pipes/fork_solved.c
+++ Lab02 - Processos/Lab02b - Pipes/fork_solved.c
@@ -13,19 +13,19 @@ int main()
   if(pipe(fd) < 0){
     printf("n foi");
   }
-	pid = fork();
+  pid = fork();
 
-	if (pid == 0) { /* child process */
-		printf("Entrei no filho!\n");
-		value += 15;
-    write(fd[1], &value, sizeof(value));
-		printf ("CHILD: value = %d\n",value); /* LINE A */
-		return 0;
-	}
-	else if (pid > 0) { /* parent process */
-		wait(NULL);
-    read(fd[0], &value, sizeof(value));
-		printf ("PARENT: value = %d\n",value); /* LINE A */
-		return 0;
-	}
+  if (pid == 0) { /* child process */
+  	printf("Entrei no filho!\n");
+	value += 15;
+    	write(fd[1], &value, sizeof(value));
+	printf ("CHILD: value = %d\n",value); /* LINE A */
+	return 0;
+  }
+  else if (pid > 0) { /* parent process */
+    	wait(NULL);
+    	read(fd[0], &value, sizeof(value));
+	printf ("PARENT: value = %d\n",value); /* LINE A */
+	return 0;
+  }
 }
